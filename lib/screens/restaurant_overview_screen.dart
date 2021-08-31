1f4d70f64a52a05588176650a736033b5226f5a4
Naufal Rajabi <abinaufal71@gmail.com>
2021-08-31 17:08:22 +0700

    Fix overview layout.

--- lib/screens/restaurant_overview_screen.dart
+++ lib/screens/restaurant_overview_screen.dart
@@ -11,14 +11,12 @@ class RestaurantOverviewScreen extends StatelessWidget {
       builder: (context, snapshot) {
         final List<Restaurant> restos =
             parseRestaurant(snapshot.hasData ? snapshot.data : "");
-        return Container(
-          height: 100,
-          child: ListView.builder(
-            itemCount: restos.length,
-            itemBuilder: (context, index) {
-              return _buildRestoItem(context, restos[index]);
-            },
-          ),
+        return ListView.builder(
+          padding: EdgeInsets.all(10.0),
+          itemCount: restos.length,
+          itemBuilder: (context, index) {
+            return _buildRestoItem(context, restos[index]);
+          },
         );
       },
     );
@@ -28,37 +26,24 @@ class RestaurantOverviewScreen extends StatelessWidget {
     return ListTile(
       leading: ClipRRect(
         borderRadius: BorderRadius.circular(10.0),
-        child: Image.network(resto.pictureId),
+        child: Image.network(
+          resto.pictureId,
+          width: 70,
+          fit: BoxFit.cover,
+        ),
       ),
       title: Text(resto.name),
-      subtitle: Text(resto.description),
+      subtitle: Text(
+        resto.description,
+        overflow: TextOverflow.ellipsis,
+      ),
     );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Restaurant'),
-                SizedBox(
-                  height: 16,
-                ),
-                Text('Recommendation restaurant for you'),
-                SizedBox(
-                  height: 16,
-                ),
-                _buildList(context),
-              ],
-            ),
-          ),
-        ),
-      ),
+      body: _buildList(context),
     );
   }
 }
